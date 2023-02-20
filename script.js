6de0dd7db54082f4b4c15d5fb174250e3e31af1e
Nadi Erofeeva <n.erofeeva72@gmail.com>
2023-02-21 00:38:41 +0500

    15 урок сложный вариант

--- script.js
+++ script.js
@@ -1,36 +1,78 @@
 'use strict';
 
-const DomElement = function (selector, height, width, bg, fontSize) {
+const DomElement = function (selector, height, width, bg, fontSize, css = '') {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    this.css = css;
 
+    this.element = undefined;
     this.createDomElement = function () {
         if (this.selector[0] === '.' || this.selector[0] === '#') {
-            let element;
             if (this.selector[0] === '.') {
-                element = document.createElement('div');
-                element.classList = this.selector.substring(1);
+                this.element = document.createElement('div');
+                this.element.classList = this.selector.substring(1);
             } else if (this.selector[0] === '#') {
-                element = document.createElement('p');
-                element.id = this.selector.substring(1);
+                this.element = document.createElement('p');
+                this.element.id = this.selector.substring(1);
             }
-            element.style.cssText = `
+            this.element.style.cssText = `
             height: ${this.height};
             width: ${this.width};
             background: ${this.bg};
             font-size: ${this.fontSize};
+            ${this.css}
             `;
-            document.body.before(element);
+            document.body.before(this.element);
+        }
+    }
+
+    this.up = function () {
+        if (isNaN(parseInt(this.element.style.top))) {
+            this.element.style.top = '-10px';
+        } else {
+            this.element.style.top = (parseInt(this.element.style.top) - 10) + 'px';
         }
 
     }
+    this.left = function () {
+        if (isNaN(parseInt(this.element.style.left))) {
+            this.element.style.left = '-10px';
+        } else {
+            this.element.style.left = (parseInt(this.element.style.left) - 10) + 'px';
+        }
+    }
+    this.down = function () {
+        if (isNaN(parseInt(this.element.style.top))) {
+            this.element.style.top = '10px';
+        } else {
+            this.element.style.top = (parseInt(this.element.style.top) + 10) + 'px';
+        }
+    }
+    this.right = function () {
+        if (isNaN(parseInt(this.element.style.left))) {
+            this.element.style.left = '10px';
+        } else {
+            this.element.style.left = (parseInt(this.element.style.left) + 10) + 'px';
+        }
+    }
 }
 
-const newElem = new DomElement('.div', '30px', '50px', 'red', '16px');
+document.addEventListener("DOMContentLoaded", function () {
+    const newElem = new DomElement('.div', '100px', '100px', 'red', '16px', 'position: absolute;');
+    newElem.createDomElement();
 
-newElem.createDomElement();
-
-console.log(newElem);
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'ArrowUp') {
+            newElem.up();
+        } else if (e.key === 'ArrowLeft') {
+            newElem.left();
+        } else if (e.key === 'ArrowDown') {
+            newElem.down();
+        } else if (e.key === 'ArrowRight') {
+            newElem.right();
+        }
+    })
+});
